3522db12f7fb1302dee48538a2214752053f592b
aslanalper2516 <aslanalper2516@gmail.com>
2025-09-20 17:39:45 +0300

    commit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
-const PORT = 3000;
+const PORT = 3002;
 
 // Public klasörünü statik dosya olarak aç
 app.use(express.static(path.join(__dirname, "public")));
